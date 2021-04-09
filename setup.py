aefd059d36ad7e8bb81e218c30b3f61983d60b5f
toutpuissantged <amoussougedeon13@gmail.com>
2021-04-09 12:16:27 +0000

    v1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    
+
 }
 # GUI applications require a different base on Windows (the default is for
 # a console application).
@@ -11,6 +11,6 @@ build_exe_options = {
 base = "Win32GUI"
 setup( name = "editorplus",
 version = "0.1",
-description = "My GUI application!",
+description = "Text editor Programme",
 options = {"build_exe": build_exe_options},
 executables = [Executable("main.py", base=base)])
